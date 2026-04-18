4c60f51498834d542b14fda31b88402b55306b52
thecodemaster <mohdk6330@gmail.com>
2026-04-18 19:23:04 +0530

    Fix Vite config ESM bug

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import { resolve } from 'path'
+import { resolve, dirname } from 'path'
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   build: {
